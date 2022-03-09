e57f61718b06ae63a6625a10333ddd2b120f6d56
Tom Sacher <t.sacher@instamotion.com>
2022-03-09 18:20:25 +0100

    ci: more fixes

--- src/helpers/getScopes.ts
+++ src/helpers/getScopes.ts
@@ -1,11 +1,13 @@
 import * as core from '@actions/core'
 
 export function getScopes(): string[] {
-  const scopes = core.getInput('scopes').split(',')
+  const scopesInput = core.getInput('scopes');
+  if(!scopesInput) {
+    core.setFailed('no scopes provided. got ' + scopesInput);
+  }
+  const scopes = scopesInput.split(',');
   if (typeof scopes !== 'object' || !scopes.length) {
-    throw new Error(
-      `scopes are valid. Expected array of strings, got "${scopes}". Use commas to provide multiple scopes: scope1,scope2`
-    )
+    core.setFailed(`scopes are valid. Expected array of strings, got "${scopes}". Use commas to provide multiple scopes: scope1,scope2`);
   }
   return scopes
 }
